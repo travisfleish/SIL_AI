8e4168b4482146bf0fcf7c67042ba017d671deaa
travisfleish <travis.fleisher@gmail.com>
2025-04-10 15:29:42 -0400

    Removed mobile animations.

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
